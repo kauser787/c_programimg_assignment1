74dde30425c3ae58d3e90f9e3f96cea970956321
kauserahmed11 <kauserahmed201103@gmail.com>
2023-10-25 21:24:05 +0600

    c first assigntment

---
+++ assignment1.c
@@ -0,0 +1,79 @@
+
+
+// #include <stdio.h>
+// int main (){
+//     long long int  a,b;
+//     scanf("%lld %lld", &a,&b);
+//     long long int sum = a*b;
+//     printf("%lld",sum);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main (){
+//     int x;
+//     scanf("%d",&x);
+//     if(x%3==0){
+//         printf("Yes");
+//     }
+//     else
+//     {
+//         printf("NO");
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main (){
+//     int i,n;
+//     scanf("%d",&n);
+//     for (i = 1; i <= n;i++)
+//     {
+//         if (i % 3 == 0 && i % 7 == 0)
+//         {
+//             printf("%d\n", i);
+//         }
+        
+//     }
+
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main (){
+//     int tk;
+//     scanf("%d", &tk);
+//     // printf("%d",tk);
+//     if (tk>1000)
+//     {
+//         printf("I will buy Punjabi\n");
+//         if (tk >= 1500)
+//         {
+//             printf("I will buy new shoes\nAlisa will buy new shoes");
+//         }
+//     }
+//     else
+//     {
+//         printf("Bad luck!");
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+//     int main()
+//     {
+
+//         int n;
+//         scanf("%d", &n);
+//         for (int i = 1; i <= n; i++)
+//         {
+//             if (i % 3 == 0 && i % 7 == 0)
+//             {
+//                 printf("%d\n", i);
+//             }
+//         }
+//         return 0;
+// }
